71a9eb76d6932b796011b6fac411eb5bee272deb
paunchymonk <gregory@dickison.net>
2024-04-18 17:40:34 -0700

    Add (tabs), (auth), and search folders and files, create tab layout and tabs screen

--- app/index.jsx
+++ app/index.jsx
@@ -7,7 +7,7 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className='text-3xl font-pblack'>Hello C-Works!</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" className='text-xl font-plight text-blue-600'>Go to Profile</Link>
+      <Link href="/home" className='text-xl font-plight text-blue-600'>Go to Home</Link>
     </View>
   );
 }
